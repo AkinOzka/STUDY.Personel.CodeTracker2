f470c50aa8007230e873fd6ab3e2596285c0b393
akino <akino@DESKTOP-LQ8220V>
2023-03-06 18:19:22 +0100

    Revert "Keeping one message"
    This reverts commit 02d0df3cf0090b9577473d16e7b017454fb628c3.

--- CodeTracker2/Program.cs
+++ CodeTracker2/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, .Net Academy! One more modofication");
-
+Console.WriteLine("It is not so complicated to learn Git in the beginning");
